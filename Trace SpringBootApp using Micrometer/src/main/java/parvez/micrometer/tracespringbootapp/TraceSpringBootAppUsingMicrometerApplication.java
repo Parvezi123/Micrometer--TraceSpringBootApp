d0b6a28b9dfb5e1d8ecf09fb588f36aaf1dd7eb7
Parvez Alam <parvezimohammed@gmail.com>
2023-04-04 01:34:09 +0530

    Added Initial Setup and Zipkin

---
+++ Trace SpringBootApp using Micrometer/src/main/java/parvez/micrometer/tracespringbootapp/TraceSpringBootAppUsingMicrometerApplication.java
@@ -0,0 +1,13 @@
+package parvez.micrometer.tracespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceSpringBootAppUsingMicrometerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceSpringBootAppUsingMicrometerApplication.class, args);
+    }
+
+}
---
+++ Trace SpringBootApp using Micrometer/src/test/java/parvez/micrometer/tracespringbootapp/TraceSpringBootAppUsingMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package parvez.micrometer.tracespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceSpringBootAppUsingMicrometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
